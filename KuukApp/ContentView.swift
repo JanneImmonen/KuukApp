7d26dacd5b6302f8fa870ab885c7167f2d04aab1
Janne Immonen <immonenjanne@icloud.com>
2022-02-18 17:40:21 +0200

    Initial Commit

---
+++ KuukApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KuukApp
+//
+//  Created by Janne Immonen on 18.2.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KuukApp/KuukAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuukAppApp.swift
+//  KuukApp
+//
+//  Created by Janne Immonen on 18.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct KuukAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
